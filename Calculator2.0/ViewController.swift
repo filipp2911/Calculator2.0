56eb9c02a1da5e2800d219b2c0ae69d6f7fad2c9
Filipp Krupnov <philkrupnov@gmail.com>
2020-10-16 09:34:11 +0600

    Initial Commit

---
+++ Calculator2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator2.0
+//
+//  Created by Filipp Krupnov on 10/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
