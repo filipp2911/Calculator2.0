acd8f5a8cd11825aa57a595c2bb5aecca5c6e1df
Filipp Krupnov <philkrupnov@gmail.com>
2020-11-04 23:12:44 +0600

    Final

--- Calculator2.0/ViewController.swift
+++ Calculator2.0/ViewController.swift
@@ -9,7 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
         
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = display!.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            userIsInTheMiddleOfTyping = true
+            display.text = digit
+        }
+    }
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var idea = CalcWork()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            idea.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            idea.performOperation(mathematicalSymbol)
+        }
+        if let result = idea.result {
+            displayValue = result
+        }
+    }
+    
+    @IBAction func returnToZero(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        display.text = "0"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
